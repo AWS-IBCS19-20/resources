360cab42a82acdad8099f6a0a97fea68df2cc56f
Zack Considine <zack_considine@m015354.local>
2020-05-01 17:05:16 -0700

    added recursive function practice

---
+++ recursiveFunctions.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class recursiveFunctions{
+
+public recursiveFunctions() {
+
+}
+
+public Integer bunnyEars(int n){
+  return n;
+}
+
+public Integer triangle(int r){
+  return r;
+}
+
+public Integer tree(int h){
+  return h;
+}
+
+public static void main(String[] args) {
+
+
+  int inputVal = 4;
+
+  //System.out.println(bunnyEars(inputVal));
+  //System.out.println(triangle(inputVal));
+  //System.out.println(tree(inputVal));
+
+}
+}
