a0a0aaf2168f8e4a27182e411878426922569d45
Zack Considine <zack_considine@m015354.local>
2020-05-05 17:05:06 -0700

    added new recursive mini project

---
+++ recursiveBacktracking.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class recursiveBacktracking{
+
+public recursiveBacktracking() {
+
+}
+
+public boolean groupSum(int start, int[] a, int s){
+
+
+  return false;
+}
+
+public static void main(String[] args) {
+
+  recursiveBacktracking r = new recursiveBacktracking();
+
+  int start = 0;
+  int[] a = {3,5,6};
+  int s = 10;
+
+  System.out.println(r.groupSum(start,a,s));
+
+}
+}
